ed4bd0808ba5ff81cbe146a1e84c9b3148935b91
brunordgsmrr <bruno_rdgs_@hotmail.com>
2025-09-05 18:28:39 -0300

    feat: cria a aplicação backend e configura o spring

---
+++ backend/src/test/java/com/fatec_lab_eng/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec_lab_eng.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
