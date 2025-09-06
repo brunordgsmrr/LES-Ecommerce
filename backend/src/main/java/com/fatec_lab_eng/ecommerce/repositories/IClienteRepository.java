ae92887aedfc2747a50c5df928976e7cc50ff833
Bruno Rodrigues <bruno_rdgs_@hotmail.com>
2025-09-06 16:29:07 -0300

    refactor: add Annotations

--- backend/src/main/java/com/fatec_lab_eng/ecommerce/repositories/IClienteRepository.java
+++ backend/src/main/java/com/fatec_lab_eng/ecommerce/repositories/IClienteRepository.java
@@ -1,9 +1,11 @@
 package com.fatec_lab_eng.ecommerce.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.fatec_lab_eng.ecommerce.entities.Cliente;
 
+@Repository
 public interface IClienteRepository extends JpaRepository<Cliente, Long>{
 
 }
